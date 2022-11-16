dbe7b58dd963008f23a20f24a5fa000a0e42c6ff
SergevninDmitry <71733518+SergevninDmitry@users.noreply.github.com>
2022-11-16 11:20:00 +0300

    working protobuf

--- server/server2.py
+++ server/server2.py
@@ -1,10 +1,12 @@
-# 1, библиотека, на которую нужно ссылаться
 from twisted.internet import protocol, reactor
 from twisted.internet.protocol import connectionDone
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet.defer import Deferred
 # TCP4ServerEndpoint требуется win32api
 from datetime import datetime
+import tcp_connection_pb2
+
+message = tcp_connection_pb2.WrapperMessage()
 
 
 # 2, определить класс протокола на стороне сервера
@@ -25,35 +27,47 @@ class TSServerProtocol(protocol.Protocol):
         print("Соединение с% s" % (self.clientInfo))
 
     def dataReceived(self, data):
-        recData = data.decode('utf-8')
-        # print("Получены данные от % s: % s" % (self.clientInfo, recData))
-        print(recData)
-        if recData == 'amount':  # условное обозначение
-            data = str(len(self.users))
-            self.send_message(data)
-            #self.transport.write(str(len(self.users)).encode('utf-8'))
-        elif recData == 'exit':
-            reason = 'Пользователь вышел'
-            self.connectionLost(self, reason)
-        else:  # блок try except временно
-            try:
-                data, delay = recData.split()
-                time = datetime.now().strftime("%Y%m%dT%H%M%S.%f")
-                deferred = Deferred()
-                deferred.addCallback(self.send_message)
-                reactor.callLater(int(delay), deferred.callback, time)
-            except:
-                self.send_message('что-то не так(передай данные нормально)')
-                # self.send_message(time)
-            # self.transport.write(time.encode('utf-8'))
+        message.ParseFromString(data)
+        print(message)
+
+        if message.HasField('request_for_fast_response'):
+            self.fast_response()
+            # отвечать на  RequestForFastResponse сообщением  FastResponse
+            # с текущем временем на машине сервера
+        elif message.HasField('request_for_slow_response'):
+            self.slow_response(message)
+        else:
+            # тут будет ошибка
+            print("что-то плохо мне")
+        message.Clear()
+
+    def slow_response(self, data):
+        client_count = len(self.users)
+        delay = data.request_for_slow_response.time_in_seconds_to_sleep
+        instance = tcp_connection_pb2.SlowResponse()
+        instance.connected_client_count = client_count
+
+        msg = tcp_connection_pb2.WrapperMessage()
+        msg.slow_response.CopyFrom(instance)
+        # sleep
+        deferred = Deferred()
+        deferred.addCallback(self.send_message)
+        reactor.callLater(delay, deferred.callback, msg)
+
+    def fast_response(self):
+        time_now = datetime.now().strftime("%Y%m%dT%H%M%S.%f")
+        message.fast_response.current_date_time = time_now
+        self.send_message(message)
+        message.Clear()
 
     def send_message(self, msg):
-        self.transport.write(msg.encode('utf-8'))
+        self.transport.write(msg.SerializeToString())  # utf-8??
 
     def connectionLost(self, reason=connectionDone):
         # TODO: логи
         print('Delete user from server')
         self.users.remove(self)
+
 
 # 3. Определите класс фабрики на стороне сервера
 class TSServerFactory(protocol.Factory):
